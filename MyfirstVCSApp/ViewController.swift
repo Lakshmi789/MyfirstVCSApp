7aa5e2a73dc46036f9c851d3abd978c3f6d23397
Anvitha <Anvitha@anvithas-macbook-air.local>
2020-06-01 17:44:48 +0530

    Autolayouts in Storyboard

--- MyfirstVCSApp/ViewController.swift
+++ MyfirstVCSApp/ViewController.swift
@@ -19,7 +19,7 @@ class ViewController: UIViewController {
         mylbl.textColor = UIColor.red
         mylbl.layer.cornerRadius = 1.0
         mylbl.layer.borderColor = UIColor.red.cgColor
-        self.view.addSubview(mylbl)
+       // self.view.addSubview(mylbl)
         
     }
 
