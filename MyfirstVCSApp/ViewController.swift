245861fbac6982a37b6c8dbcf2d7a247e91bbd63
Anvitha <Anvitha@anvithas-macbook-air.local>
2020-06-01 14:22:01 +0530

    label created as programmatically

--- MyfirstVCSApp/ViewController.swift
+++ MyfirstVCSApp/ViewController.swift
@@ -14,6 +14,13 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("Welcome to Lakshmi PANDU HANY")
+        let mylbl = UILabel(frame: CGRect(x: 25, y: 35, width: 150, height: 50))
+        mylbl.text = "Welcome to spsoft"
+        mylbl.textColor = UIColor.red
+        mylbl.layer.cornerRadius = 1.0
+        mylbl.layer.borderColor = UIColor.red.cgColor
+        self.view.addSubview(mylbl)
+        
     }
 
 
